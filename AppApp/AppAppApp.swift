525474e0f65475ecf38e346e510503539d795bc5
Dylan Elliott <dylan.elliott@gmail.com>
2023-11-20 20:03:28 +1100

    Initial Commit

---
+++ AppApp/AppAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAppApp.swift
+//  AppApp
+//
+//  Created by Dylan Elliott on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
